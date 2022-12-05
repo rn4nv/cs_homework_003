1c38dc5f009a0f856c35795cb504e839a89ab488
rn4nv <dorjuan@ya.ru>
2022-12-06 00:48:02 +0700

    Completed CS task 001

---
+++ CS_task_001/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Для проверки числа на палиндром, введите его: ");
+string a = Console.ReadLine();
+int resultA = int.Parse(a);
+int digitA = int.Parse(a);
+int rev = 0;
+
+while (resultA > 0)
+{
+    int count = (resultA % 10);
+    rev = (rev * 10 + count);
+    resultA = resultA / 10;
+}
+if (digitA == rev)
+{
+    Console.WriteLine("Введенное число - палиндром!");
+}
+else
+{
+    Console.WriteLine("Введенное число - не палиндром!");
+}
