c457fe56f2de1bcae489bbe4173501d314d3510d
rn4nv <dorjuan@ya.ru>
2022-12-07 01:13:16 +0700

    Completed CS task 003

---
+++ CS_task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+double i = 1;
+
+while (i <= n)
+{
+    double result = Math.Pow(i,3);
+    Console.WriteLine(result);
+    i++;
+}
