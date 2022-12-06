8c4bf401f6b1efb9c88309fc6535a6da9e7972f7
rn4nv <dorjuan@ya.ru>
2022-12-07 00:52:02 +0700

    Completed CS task 002

---
+++ CS_task_002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Данные для точки 'А'.");
+Console.Write("Введите координату X для точки 'A': ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y для точки 'A': ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Данные для точки 'B'.");
+Console.Write("Введите координату X для точки 'B': ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y для точки 'B': ");
+int y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt((x2 - x1)+(y2 - y1));
+
+Console.Write("Расстояние между точками 'A' и 'B' равно: ");
+Console.Write(Math.Round(result, 2));
